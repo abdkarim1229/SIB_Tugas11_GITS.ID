c9a1b98a3a7d78ff705f9a9720b3156b316d298d
abdkarim1229 <abdulkarimklj@gmail.com>
2021-09-15 18:53:15 +0700

    first-commit

---
+++ app/src/main/java/com/example/sib_tugaskesebelas/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.example.sib_tugaskesebelas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        txt_signin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
